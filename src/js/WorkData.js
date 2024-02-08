ad7129b085a98b2b704d3ed8ae16a7ef9855673b
Cristobal Lira <cristoballira@MacBook-Air-de-Cristobal.local>
2024-02-08 15:27:14 -0300

    correccion en subida

--- src/js/WorkData.js
+++ src/js/WorkData.js
@@ -1,14 +1,21 @@
 const data = [
     {
+        year:2023,
+        title:'Global Reefers (Ingeniero de Datos).',
+        duration:'Trabajo Actual',
+        details:
+        'Como ingeniero de datos, superviso cambios en el sistema de documentación de BLS y colaboro estrechamente con analistas de negocio para diseñar APIs que faciliten la creación de reportes. Mi trabajo implica desarrollar soluciones técnicas para automatizar la extracción, transformación y carga de datos, así como limpiar y manipular grandes conjuntos de datos para garantizar su integridad. A través de esta colaboración interdisciplinaria, contribuyo a proporcionar información valiosa para la toma de decisiones empresariales.',
+    },
+    {
         year:2022,
-        title:'CARGONAUTS (Desarrollador FullStack)',
+        title:'Cargonauts (Desarrollador FullStack).',
         duration:'1 año 5 meses',
         details:
         'Como desarrollador de software full stack, soy el encargado de liderar el desarrollo de módulos de una app web para un agente de consolidación aéreo y marítimo. Este sistema ayuda a la empresa a organizar sus guías aéreas, operaciones marítimas, cotizaciones, entre otros. A pesar de que la empresa es una embarcadora y no tiene mucho que ver con mi área, estoy comprometido en entender el funcionamiento del negocio para entregar soluciones que tengan sentido a la hora de organizar mejor la gestión. El sistema está desarrollado en la web con tecnología Ajax para aplicaciones asíncronas, HTML, CSS, JavaScript (jQuery) y para el lado del servidor se ocupa PHP y base de datos MySQL. Además de mi rol como desarrollador, como único encargado de TI en la empresa, también brindo soporte en la oficina, como el mantenimiento y soporte de los equipos informáticos, servidor central, impresoras, contactos con proveedores externos de software y soporte de internet.',
     },
     {
         year:2022,
-        title:'Global Reefers (Proyecto Web Freelance)',
+        title:'Global Reefers (Proyecto Web Freelance).',
         duration:'7 meses',
         details:
         'Como desarrollador externo en Global Reefers, mi labor consistió en mejorar el flujo de trabajo de la empresa mediante el desarrollo de módulos de aplicaciones web. Utilicé tecnologías de consumo de servicios web y me conecté con diferentes APIs para automatizar tareas como el envío de BL a la aduana, la creación de PDF con JavaScript, la generación de XML y SMS para el envío de BL a las aduanas, entre otras. Me aseguré de cumplir con los requisitos específicos de la empresa para garantizar que las soluciones fueran efectivas y eficientes en la optimización del proceso de trabajo.',
@@ -22,7 +29,7 @@ const data = [
     },
     {
         year:2021,
-        title:'Ipsos (Programador)',
+        title:'Ipsos Programador jr',
         duration:'6 meses',
         details:'En mi rol de programador en la empresa, apliqué mis conocimientos informáticos y analíticos para entregar un servicio de alta calidad y eficiencia. Mis principales responsabilidades incluyeron programar encuestas en IBM SPSS Data Collection y recolectar datos a través de cuestionarios. Con experiencia en programación y mi habilidad para analizar datos, fui capaz de asegurar que se entregara un producto final preciso y de calidad.',
     }
